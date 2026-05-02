e3937b47329bf44ffaca9816e48bc54511603e30
zyplocs <zyplocked@gmail.com>
2026-05-01 22:19:16 -0500

    Append trailing newlines to module files + comment out unused imports in vectors3d

--- src/linalg/vectors/vectors3d.py
+++ src/linalg/vectors/vectors3d.py
@@ -1,7 +1,9 @@
 # linalg/vectors/vectors3d.py
 """A module for working with vectors of three dimensions."""
 
-from __future__ import annotations
-import math
-from ..utils import guards as gd
-from ..utils.guards import NumericTypeError, ScalarLike
+#from __future__ import annotations
+
+#import math
+
+#from ..utils import guards as gd
+#from ..utils.guards import NumericTypeError, ScalarLike
