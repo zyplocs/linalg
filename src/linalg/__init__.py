c9f64bbde50b683de862c92ee5c3287f243ccb19
zyplocs <zyplocked@gmail.com>
2026-05-04 23:57:05 -0500

    Export `Mat2` class from package-level `__init__` files

--- src/linalg/__init__.py
+++ src/linalg/__init__.py
@@ -1,9 +1,11 @@
 # linalg/__init__.py
 """Linear Algebra for Visual Applications"""
+
 from .geometry.vectors2d import Vector2D
+from .geometry.matrix2d import Mat2
 from .utils.guards import NumericTypeError
 
-__all__ = ['Vector2D', 'NumericTypeError']
+__all__ = ['Vector2D', 'Mat2', 'NumericTypeError']
 
 # Metadata
 #__version__ = '0.1.0'
