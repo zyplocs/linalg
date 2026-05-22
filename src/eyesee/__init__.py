6171fe6fe5ec03184b5bbb862e9d9927e555d3cd
zyplocs <zyplocked@gmail.com>
2026-05-21 19:36:31 -0500

    Rename package from `linalg` to `eyesee` + add `vision` subpackage to exports

--- src/eyesee/__init__.py
+++ src/eyesee/__init__.py
@@ -4,9 +4,10 @@
 from .geometry.vectors2d import Vector2D
 from .geometry.matrix2d import Mat2
 from .utils.guards import NumericTypeError
+from . import vision
 
-__all__ = ['Vector2D', 'Mat2', 'NumericTypeError']
+__all__ = ['Vector2D', 'Mat2', 'NumericTypeError', 'vision']
 
 # Metadata
-#__version__ = '0.1.0'
+#__version__ = '0.0.0-1'
 #__author__ = 'Eli M J'
